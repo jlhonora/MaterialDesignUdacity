ac5a094042d147b9fa538dd7fa746ea0f9a3e4e3
jlhonora <jlhonora@ing.puc.cl>
2015-10-29 15:39:11 -0300

    Scroll anim for API < 21, better styling

--- app/src/main/java/org/honorato/materialdesignudacity/ScrollAnimationActivity.java
+++ app/src/main/java/org/honorato/materialdesignudacity/ScrollAnimationActivity.java
@@ -2,7 +2,9 @@ package org.honorato.materialdesignudacity;
 
 import android.animation.Animator;
 import android.animation.ObjectAnimator;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ScrollView;
 
@@ -14,6 +16,8 @@ public class ScrollAnimationActivity extends AppCompatActivity {
     @Bind(R.id.scroll_view_anim)
     ScrollView mScrollView;
 
+    boolean mDidAnimateEnter = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,9 +26,32 @@ public class ScrollAnimationActivity extends AppCompatActivity {
     }
 
     @Override
-    public void onEnterAnimationComplete() {
-        super.onEnterAnimationComplete();
+    protected void onResume() {
+        super.onResume();
 
+        setupAnimation();
+    }
+
+    protected void setupAnimation() {
+        // For API level >= 21, we use
+        // onEnterAnimationComplete
+        if (Build.VERSION.SDK_INT >= 21) {
+            return;
+        }
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //Do something after 100ms
+            }
+        }, 100);
+    }
+
+    protected void performAnimation() {
+        if (mDidAnimateEnter) {
+            return;
+        }
+        mDidAnimateEnter = true;
         final int startScrollPos = getResources()
                 .getDimensionPixelSize(R.dimen.scroll_animation_start);
 
@@ -35,5 +62,11 @@ public class ScrollAnimationActivity extends AppCompatActivity {
                 .setDuration(1000);
 
         anim.start();
+    }
+
+    @Override
+    public void onEnterAnimationComplete() {
+        super.onEnterAnimationComplete();
+        performAnimation();
     }
 }
